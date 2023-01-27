a9be1e15307634fb159dadf8bea397710d6be662
NathanD0wd <dowdnj@gmail.com>
2023-01-27 12:47:40 -0800

    Fixed Bug

--- ArrayTests.java
+++ ArrayTests.java
@@ -27,4 +27,10 @@ public class ArrayTests {
     int[] input1 = { 1, 2, 3, 4, 5 };
     assertArrayEquals(new int[]{ 5, 4, 3, 2, 1 }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input = { 4, 4, 4, 6, 6};
+    assertEquals( 5.0 , ArrayExamples.averageWithoutLowest(input) , .001 );
+  }
 }
---
+++ ListTests.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListTests {
+    @Test
+    public void filter1() {
+        List<String> input1 = new ArrayList<String>();
+        input1.add("abc");
+        input1.add(null);
+        input1.add("ABC");
+        input1.add("blueberry");
+        List<String> output1 = new ArrayList<String>();
+        output1.add("abc");
+        output1.add("ABC");
+        output1.add("blueberry");
+        StringChecker scheck = new ListExamples();
+        assertEquals( output1 , ListExamples.filter( input1 , scheck ));
+    }
+
+    @Test
+    public void merge1() {
+        List<String> input1 = new ArrayList<String>();
+        List<String> input2 = new ArrayList<String>();
+        List<String> output1 = new ArrayList<String>();
+        input1.add("a");
+        input2.add("b");
+        input2.add("c");
+        input1.add("e");
+        input2.add("f");
+        input2.add("g");
+        output1.add("a");
+        output1.add("b");
+        output1.add("c");
+        output1.add("e");
+        output1.add("f");
+        output1.add("g");
+        System.out.println( "" + input1 + input2 + output1 );
+        assertEquals( output1 , ListExamples.merge(input1, input2) );
+    }
+}
