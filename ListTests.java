29bdce84ca7d93eec9f265e3f3494731fdcea154
NathanD0wd <dowdnj@gmail.com>
2023-01-27 16:54:48 -0800

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -21,22 +21,25 @@ public class ListTests {
     }
 
     @Test
-    public void merge1() {
+    public void mergeError() {
         List<String> input1 = new ArrayList<String>();
         List<String> input2 = new ArrayList<String>();
         List<String> output1 = new ArrayList<String>();
-        input1.add("a");
-        input2.add("b");
-        input2.add("c");
-        input1.add("e");
-        input2.add("f");
-        input2.add("g");
-        output1.add("a");
-        output1.add("b");
-        output1.add("c");
-        output1.add("e");
-        output1.add("f");
-        output1.add("g");
+        input1.add("a"); input1.add("e");
+        input2.add("b"); input2.add("c"); input2.add("f"); input2.add("g");
+        output1.add("a"); output1.add("b"); output1.add("c"); output1.add("e"); output1.add("f"); output1.add("g");
+        System.out.println( "" + input1 + input2 + output1 );
+        assertEquals( output1 , ListExamples.merge(input1, input2) );
+    }
+
+    @Test
+    public void mergeNoError() {
+        List<String> input1 = new ArrayList<String>();
+        List<String> input2 = new ArrayList<String>();
+        List<String> output1 = new ArrayList<String>();
+        input1.add("a"); input1.add("e"); input1.add("g");
+        input2.add("b"); input2.add("c"); input2.add("f");
+        output1.add("a"); output1.add("b"); output1.add("c"); output1.add("e"); output1.add("f"); output1.add("g");
         System.out.println( "" + input1 + input2 + output1 );
         assertEquals( output1 , ListExamples.merge(input1, input2) );
     }
